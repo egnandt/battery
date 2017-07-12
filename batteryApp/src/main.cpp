bc10b534504679b0d9fab70cd635abf2296e0627
Edvard Gnandt <edvard.gnandt@magnetimarelli.com>
2017-07-12 15:28:07 +0300

    Changed application type to QApplication

--- batteryApp/src/main.cpp
+++ batteryApp/src/main.cpp
@@ -1,11 +1,11 @@
-#include <QGuiApplication>
+#include <QApplication>
 #include <QDir>
 
 #include "batteryapplication.h"
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
     BatteryApplication battApp(QDir::cleanPath(QCoreApplication::applicationDirPath()));
     battApp.initialize();
     return app.exec();
